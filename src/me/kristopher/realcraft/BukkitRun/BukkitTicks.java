a5f361ba1d211198836d99db3475d4d87c7c7c06
Creasium <creasium@mail.ru>
2019-06-25 19:00:13 +0300

    Finding heating source near player

--- src/me/kristopher/realcraft/BukkitRun/BukkitTicks.java
+++ src/me/kristopher/realcraft/BukkitRun/BukkitTicks.java
@@ -5,7 +5,10 @@ import me.kristopher.realcraft.util.StringUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -26,14 +29,25 @@ public class BukkitTicks extends BukkitRunnable {
 		playersCooldown = new HashMap<>();
 	}
 
-	public BukkitTicks() {
-
-	}
+	public BukkitTicks() { }
+	boolean day = false; //day/night boolean
 
 	@Override
 	public void run() {
-
 		for (Player p : Bukkit.getOnlinePlayers()) {
+			World w = p.getWorld();
+
+			//day or night boolean operation
+			if(p.getWorld().getTime() < 13000 && !day)
+			{
+				day = true;
+			}
+			else if(p.getWorld().getTime() >= 13000 && day)
+			{
+				day = false;
+			}
+
+			//If player more than 200 blocks up - he we start suffocating
 			if (p.getLocation().getBlockY() >= 200) {
 				if (!p.hasPotionEffect(PotionEffectType.WATER_BREATHING)) {
 					p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 8000, 1));
@@ -41,25 +55,54 @@ public class BukkitTicks extends BukkitRunnable {
 					if (canSendMessage(p.getUniqueId()))
 						p.sendMessage(StringUtil.inColor(plugin.getMsgs().getTooHigh()));
 				}
-			} else if (p.getLocation().getBlockY() <= 200) {
+			} else if (p.getLocation().getBlockY() <= 200) { //But if lower than 200 all effects will disappear
 				p.removePotionEffect(PotionEffectType.POISON);
 				p.removePotionEffect(PotionEffectType.CONFUSION);
 			}
 
-			int radius = 8;
+			//Checking for heating sources near player
+			int radius = 5; //With a radius 5
 			Location loc = p.getLocation();
 
-			for (int x = -(radius); x <= radius; x++) {
-				for (int y = -(radius); y <= radius; y++) {
-					for (int z = -(radius); z <= radius; z++) {
-						if (loc.getBlock().getRelative(x, y, z).getType() == Material.CAMPFIRE && (x != 0 || y != 0 || z != 0)) {
-							p.sendMessage("Пошёл ты на хуй и не мёрзни");
+			if (w.hasStorm() || !day) {
+				for (int x = -(radius); x <= radius; x++) {
+					for (int y = -(radius); y <= radius; y++) {
+						for (int z = -(radius); z <= radius; z++) {
+							if (loc.getBlock().getRelative(x, y, z).getType() == Material.CAMPFIRE) {
+								p.sendMessage("5 Material.CAMPFIRE");
+								return;
+							} else if (loc.getBlock().getRelative(x, y, z).getType() == Material.LAVA) {
+								p.sendMessage("5 Material.LAVA");
+								return;
+							} else if (loc.getBlock().getRelative(x, y, z).getType() == Material.MAGMA_BLOCK) {
+								p.sendMessage("5 MAGMA_BLOCK ");
+								return;
+							}
 						}
 					}
 				}
+				int lowradius = 3; //With a radius 3
+				for (int x = -(lowradius); x <= lowradius; x++) {
+					for (int y = -(lowradius); y <= lowradius; y++) {
+						for (int z = -(lowradius); z <= lowradius; z++) {
+							if (loc.getBlock().getRelative(x, y, z).getType() == Material.TORCH && (x != 0 || y != 0 || z != 0)) {
+								p.sendMessage("3 Material.TORCH");
+								return;
+							} else if (loc.getBlock().getRelative(x, y, z).getType() == Material.LANTERN && (x != 0 || y != 0 || z != 0)) {
+								p.sendMessage("3 Material.LANTERN");
+								return;
+							}
+						}
+					}
+				}
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 80, 1));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 80, 1));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 80, 1));
+
 			}
 		}
 	}
+
 	//Delay before messages
 	private boolean canSendMessage (UUID uuid){
 		if (playersCooldown.containsKey(uuid)) {
